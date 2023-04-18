f529123a1e11ea18f2e4864a1f0a6d80a7692200
Anton Putra <me@antonputra.com>
2023-04-18 11:55:25 -0700

    asd

--- lessons/161/1-loop/1-count/main.tf
+++ lessons/161/1-loop/1-count/main.tf
@@ -1,23 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_blocks" {
-  description = "CIDR blocks for the subnets."
-  type        = list(string)
-  default     = ["10.0.0.0/19", "10.0.64.0/19"]
-}
-
-module "subnets" {
-  source = "../../modules/subnet"
-
+resource "aws_subnet" "example" {
   vpc_id = aws_vpc.main.id
 
-  count             = length(var.subnet_cidr_blocks)
-  subnet_cidr_block = var.subnet_cidr_blocks[count.index]
-}
-
-output "all_ids" {
-  value       = module.subnets[*].subnet_id
-  description = "The IDs for all subnets."
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "us-east-1a"
 }
