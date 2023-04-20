7b1984e2134d53ce8c9baed6a2ccb13cbac34395
Anton Putra <me@antonputra.com>
2023-04-20 08:41:49 -0700

    asd

--- lessons/161/1-loop/2-for_each/main.tf
+++ lessons/161/1-loop/2-for_each/main.tf
@@ -1,18 +1,18 @@
 variable "subnet_cidr_blocks" {
   description = "CIDR blocks for the subnets."
   type        = list(string)
-  default     = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
+  default     = ["10.0.0.0/19", "10.0.64.0/19"]
 }
 
-resource "aws_subnet" "example" {
+module "subnets" {
+  source   = "../../modules/subnet"
   for_each = toset(var.subnet_cidr_blocks)
 
-  vpc_id = aws_vpc.main.id
-
-  cidr_block        = each.value
-  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.main.id
+  subnet_cidr_block = each.value
 }
 
-output "all_ids" {
-  value = values(aws_subnet.example)[*].id
+output "subnet_ids" {
+  value       = values(module.subnets)[*].subnet_id
+  description = "The IDs of the created subnets."
 }
